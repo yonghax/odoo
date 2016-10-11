b0b1c9de2d87b5feb5e63f23d1aa9d81a109b5c7
yonghax <yohanes.ho@sociolla.com>
2016-10-11 12:09:59 +0700

    init product bundle

--- addons/sociolla/models/product_bundle.py
+++ addons/sociolla/models/product_bundle.py
@@ -2,15 +2,51 @@ from openerp.osv import fields, osv
 from openerp import api, fields, models, _
 from openerp.exceptions import UserError
 
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+    
+    is_product_bundle = fields.Boolean(
+        string='Product Bundle',
+        required=False,
+        default=False,
+        help="Specify if the product is pack bundle"
+    )
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+    
+    product_bundle = fields.One2many(
+        'product.bundle', 
+        'parent_product_id', 
+        string='Parent Product', 
+        copy=True
+    )
+
 class ProductBundle(models.Model):
     _name = 'product.bundle'
 
-    product_id = fields.Many2one(
-        'product.product',
-        string='Product',
-        required=True,
-        ondelete='cascade',
-        help='Select product to add this bundle',
+    parent_product_id = fields.Many2one(
+        'product.product', 
+        string='Parent Product', 
+        required=True, 
+        ondelete='cascade', 
         index=True, 
         copy=False
+    )
+
+    product_id = fields.Many2one(
+        'product.product', 
+        string='Product', 
+        domain=[('sale_ok', '=', True)], 
+        change_default=True, 
+        ondelete='restrict', 
+        required=True
+    )
+    
+    qty = fields.Integer(
+        string='Quantity',
+        required=True,
+        readonly=False,
+        default=1,
+        help=False
     )
