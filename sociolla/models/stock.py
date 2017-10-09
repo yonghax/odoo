d21eadef2c3384933cbea8fe2e3c091fb9546fe5
yonghax <yohanes.ho@sociolla.com>
2017-10-09 13:15:55 +0700

    add link warehouse_location on stock_pack_ops

--- sociolla/models/stock.py
+++ sociolla/models/stock.py
@@ -144,11 +144,13 @@ class stock_inventory_line(models.Model):
 class stock_pack_operation(models.Model):
     _inherit = ['stock.pack.operation']
     barcode = fields.Char(string=u'Barcode',compute='_get_barcode')
+    warehouse_location = fields.Char(string=u'Location',compute='_get_barcode')
     
     def _get_barcode(self):
         for data in self:
             if data.product_id:
                 data.barcode = data.product_id.barcode
+                data.warehouse_location = data.product_id.product_tmpl_id.warehouse_location
 
 class stock_move(models.Model):
     _inherit = 'stock.move'
