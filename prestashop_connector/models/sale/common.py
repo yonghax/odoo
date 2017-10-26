6a49d312c83a7c4f2aff49372160008b47e34fd4
yonghax <yohanes.ho@sociolla.com>
2017-10-26 16:45:21 +0700

    fix issue order discount voucher

--- prestashop_connector/models/sale/common.py
+++ prestashop_connector/models/sale/common.py
@@ -335,12 +335,16 @@ select op.id_cart, o.id_order, o.date_add, o.reference as reference_order, o.cur
         """ Delete order line with product discount. and the amount will be split average per order line.
         """
         order_lines = sale_order.order_line
-        order_discounts = order_lines.filtered(lambda x: x.product_id == self.backend_record.discount_product_id)
+        order_discounts = order_lines.filtered(lambda x: x.product_id.id == self.backend_record.discount_product_id.id)
         order_products = order_lines.filtered(lambda x: x.product_id != self.backend_record.discount_product_id and x.product_id.type == 'product')
         order_products = sorted(order_products, key=lambda x : x.price_total, reverse=True)
-        
-        sum_discount_amount = sum([x.price_unit for x in order_discounts])
+
+        sum_discount_amount = float(self.prestashop_record['total_discounts'])
         sum_total_amount_header = sum([x.price_total for x in order_products])
+        print 'order_discounts', len(order_discounts)
+        print 'sum_discount_amount', sum_discount_amount
+        
+        
         if sum_discount_amount > 0:
             for i in xrange(0, len(order_products)):
                 total_amount = order_products[i].price_total
@@ -349,8 +353,6 @@ select op.id_cart, o.id_order, o.date_add, o.reference as reference_order, o.cur
                      discount_header_amount = total_amount
 
                 order_products[i]._compute_proportional_amount(discount_header_amount)
-        
-            order_discounts.unlink()
 
         sale_order.discount_amount = sum_discount_amount
         sale_order.update({
