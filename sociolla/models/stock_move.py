cae9b040185533b112c9af8c8e6c4ef51475de82
yonghax <yohanes.ho@sociolla.com>
2017-12-04 16:10:27 +0700

    add new feature: - adjust inv out product consignment will create journal - stock move add mapping with inventory adjustment line - add purchase validator wizard -

--- sociolla/models/stock_move.py
+++ sociolla/models/stock_move.py
@@ -6,3 +6,5 @@ class stock_move(models.Model):
     is_switchover_stock = fields.Boolean(string='Switchover stock')
     is_gwp_free = fields.Boolean(string=u'Free Product',default=False,)
     source_date = fields.Datetime(string=u'Source Date',)
+    inventory_line_id = fields.Many2one(string=u'Inventory Line ID',comodel_name='stock.inventory.line',)
+    
